12b0f82d977e85bb5e77f15697fef8e489f4d15a
Torrin Curry <tc89585@uga.edu>
2023-10-02 10:14:28 -0400

    create movie and get all movies implemented as well as connection to amazon rds

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,83 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+/*connect to database*/
+
+const db = mysql.createConnection({
+  host: 'cinema-booking-db.cexoykt5nvcs.us-east-2.rds.amazonaws.com',
+  port: '3306',
+  user: 'admin',
+  password: 'password',
+  database: 'mydb',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Database connected');
+});
+
+app.get('/movies', async (req, res) => {
+  db.query('SELECT * FROM movie', (err, results) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(results);
+      res.json(results);
+    }
+  });
+});
+
+app.post('/createMovie', (req, res) => {
+  const {
+    title,
+    category,
+    cast,
+    director,
+    producer,
+    synopsis,
+    reviews,
+    trailer_picture,
+    trailer_video,
+    mpaa_rating,
+    show_dates_times,
+  } = req.body;
+
+  const query = `INSERT INTO movie (title, category, cast, director, producer, synopsis, reviews, trailer_picture, trailer_video, mpaa_rating, show_dates_times) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+  db.query(
+    query,
+    [
+      title,
+      category,
+      cast,
+      director,
+      producer,
+      synopsis,
+      reviews,
+      trailer_picture,
+      trailer_video,
+      mpaa_rating,
+      show_dates_times,
+    ],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error creating the movie' });
+      } else {
+        res.status(200).json({ message: 'Movie created successfully' });
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
