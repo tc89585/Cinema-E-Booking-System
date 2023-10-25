61a8867d2e3d9bc1a7b2adf0e6ef2966c6a01bd5
ofuan1 <ofuan.oyakhire@gmail.com>
2023-10-25 19:44:42 -0400

    app and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Components/Home'; 
-import Login from './Components/Login'; 
-import Checkout from './Components/Checkout'; 
-import Book from './Components/Book'; 
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Checkout from './Components/Checkout';
+import Book from './Components/Book';
+import AdminDashboard from './Components/AdminDashboard';
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/Login" element={<Login/>} />
+        {/* User routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/Checkout" element={<Checkout />} />
-        <Route path="/Book" element={<Book/>} />
+        <Route path="/Book" element={<Book />} />
+        <Route path="/admin/*" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react';
 import './Login.css';
 
 function Login() {
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [subscribeToPromotions, setSubscribeToPromotions] = useState(false);
   const [isLogin, setIsLogin] = useState(true);
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -14,16 +23,38 @@ function Login() {
     setPassword(e.target.value);
   };
 
+  const handlePasswordConfirmChange = (e) => {
+    setPasswordConfirm(e.target.value);
+  };
+
+  const handlePromotionsChange = (e) => {
+    setSubscribeToPromotions(e.target.checked);
+  };
+
   const handleFormToggle = () => {
     setIsLogin(!isLogin);
+    setErrorMessage('');
+    setIsRegistered(false);
+    // Clear form fields here if needed
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (isLogin) {
       // Add your login logic here
+      setErrorMessage('');
     } else {
-      // Add your sign-up logic here
+      // Form validation
+      if (!username || !email || !password || !passwordConfirm) {
+        setErrorMessage('Please fill in all mandatory fields.');
+      } else if (password !== passwordConfirm) {
+        setErrorMessage('Password and confirm password must match.');
+      } else {
+        // Registration successful
+        setIsRegistered(true);
+        setErrorMessage('');
+        // Clear form fields or navigate to a confirmation page
+      }
     }
   };
 
@@ -32,6 +63,17 @@ function Login() {
       <div className={`login-form ${isLogin ? 'login-form-login' : 'login-form-signup'}`}>
         <h2>{isLogin ? 'Login' : 'Sign Up'}</h2>
         <form onSubmit={handleSubmit}>
+          {!isLogin && (
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={handleUsernameChange}
+                required
+              />
+            </div>
+          )}
           <div className="form-group">
             <input
               type="email"
@@ -50,10 +92,35 @@ function Login() {
               required
             />
           </div>
+          {!isLogin && (
+            <div className="form-group">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                value={passwordConfirm}
+                onChange={handlePasswordConfirmChange}
+                required
+              />
+            </div>
+          )}
+          <div className="form-group">
+            <label>
+              <input
+                type="checkbox"
+                checked={subscribeToPromotions}
+                onChange={handlePromotionsChange}
+              />
+              Subscribe to Promotions
+            </label>
+          </div>
           <button type="submit" className="login-button">
             {isLogin ? 'Login' : 'Sign Up'}
           </button>
         </form>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        {isRegistered && (
+          <div className="confirmation-message">Registration successful! You can now log in.</div>
+        )}
         <p>
           {isLogin ? "Don't have an account? " : 'Already have an account? '}
           <span className="toggle-link" onClick={handleFormToggle}>
