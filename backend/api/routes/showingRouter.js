dc58d8834433ab476e3fdae6f149313676fbfa6a
tc89585 <90994770+tc89585@users.noreply.github.com>
2023-11-30 20:25:50 -0500

    Delete backend/api/routes/showingRouter.js

--- backend/api/routes/showingRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ShowingController = require('../controllers/ShowingController');
-
-router.get('/showings/:showingId/available-seats', ShowingController.getAvailableSeats);
-
-module.exports = router;
