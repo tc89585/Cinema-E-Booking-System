7e0a91215a6ebaea9015bf5c47063f56dc45f1b2
Fittede <ejoseph13@icloud.com>
2023-12-04 01:08:00 -0500

    feat: add movies, and change status

--- frontend/src/Components/Admin/AdminDashboard.js
+++ frontend/src/Components/Admin/AdminDashboard.js
@@ -1,38 +1,4 @@
-// import React from 'react';
-// import { Link, Route, Routes} from 'react-router-dom';
-// import ManageMovies from './ManageMovies';
-// import ManageUsers from './ManageUsers';
-// import ManagePromotions from './ManagePromotions';
-// import './AdminDashboard.css';
 
-// function AdminDashboard() {
-//   return (
-//     <div className="admin-dashboard">
-//       <nav className="admin-nav">
-//         <ul>
-//           <li>
-//             <Link to="/admin/movies">Manage Movies</Link>
-//           </li>
-//           <li>
-//             <Link to="/admin/users">Manage Users</Link>
-//           </li>
-//           <li>
-//             <Link to="/admin/promotions">Manage Promotions</Link>
-//           </li>
-//         </ul>
-//       </nav>
-//       <div className="admin-content">
-//         <Routes>
-//           <Route path="/admin/movies" component={ManageMovies} />
-//           <Route path="/admin/users" component={ManageUsers} />
-//           <Route path="/admin/promotions" component={ManagePromotions} />
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default AdminDashboard;
 
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
@@ -48,13 +14,13 @@ function AdminDashboard() {
           <nav>
             <ul className="admin-menu">
               <li>
-                <Link to="movies">Manage Movies</Link>
+              <Link to="/admin/movies">Manage Movies</Link>
               </li>
               <li>
-                <Link to="users">Manage Users</Link>
+                <Link to="/admin/users">Manage Users</Link>
               </li>
               <li>
-                <Link to="promotions">Manage Promotions</Link>
+                <Link to="/admin/promotions">Manage Promotions</Link>
               </li>
             </ul>
           </nav>
