d8423055f093bb948989202ff87a404eaec945b0
JackAlexander12 <kakojacko@gmail.com>
2023-12-03 19:25:37 -0500

    loginMethod Update and getMovieById

--- backend/api/controllers/movieController.js
+++ backend/api/controllers/movieController.js
@@ -9,6 +9,20 @@ const getAllMovies = async (req, res) => {
   }
 };
 
+const getMovieById = async (req, res) => {
+  const { movie_id } = req.params;
+  try {
+    const movie = await Movie.findByPk(movie_id);
+    if (!movie) {
+      return res.status(404).send({ message: 'Movie not found' });
+    }
+    res.status(200).send(movie);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllMovies,
+  getMovieById
 };
